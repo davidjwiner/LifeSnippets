ddabac1a52e000df9b0ab1f52d741693ef248be2
David Winer <dwiner@google.com>
2020-01-03 16:34:54 -0800

    Initial commit

---
+++ app/src/main/java/com/lifesnippets/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.lifesnippets.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/lifesnippets/ui/main/NoteViewModel.kt
@@ -0,0 +1,7 @@
+package com.lifesnippets.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class NoteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
